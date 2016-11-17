4fefa0d63d850d1056515395740bca72bc7b2d13
emanzelekha <emanzelekha@gmail.com>
2016-11-17 15:33:41 +0200

    login-r

---
+++ app/src/main/java/com/example/ok/madicalalatheer/Main/Login.java
@@ -0,0 +1,15 @@
+package com.example.ok.madicalalatheer.Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ok.madicalalatheer.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
